364dc6bcec20c681928b39e372599552010ed40b
analyticsmaleoGod <analytics.maleo@futurecreativenetwork.com>
2026-05-01 12:44:33 +0700

    facebook.py

--- platforms/facebook.py
+++ platforms/facebook.py
@@ -11,14 +11,15 @@ class FacebookScraper(BaseScraper):
 
     def build_input(self, username: str, date_from: datetime, date_to: datetime) -> dict:
         clean = username.strip().strip("/")
-        # Support both page name and full URL
         if "facebook.com" not in clean:
             url = f"https://www.facebook.com/{clean}"
         else:
             url = clean if clean.startswith("http") else f"https://{clean}"
         return {
-            "startUrls": [{"url": url}],
-            "resultsLimit": self.smart_limit(date_from, date_to),
+            "startUrls":            [{"url": url}],
+            "resultsLimit":         self.smart_limit(date_from, date_to),
+            "onlyPostsNewerThan":   date_from.strftime("%Y-%m-%d"),
+            "onlyPostsOlderThan":   date_to.strftime("%Y-%m-%d"),
         }
 
     def parse_results(self, raw_items: list, date_from: datetime, date_to: datetime) -> list[dict]:
@@ -32,10 +33,10 @@ class FacebookScraper(BaseScraper):
             if not (date_from <= post_date <= date_to):
                 continue
 
-            likes = self.safe_int(item.get("likesCount") or item.get("likes") or item.get("reactions"))
+            likes    = self.safe_int(item.get("likesCount") or item.get("likes") or item.get("reactions"))
             comments = self.safe_int(item.get("commentsCount") or item.get("comments"))
-            shares = self.safe_int(item.get("sharesCount") or item.get("shares"))
-            views = self.safe_int(item.get("viewsCount") or item.get("views", 0))
+            shares   = self.safe_int(item.get("sharesCount") or item.get("shares"))
+            views    = self.safe_int(item.get("viewsCount") or item.get("views", 0))
 
             text = item.get("text") or item.get("message") or item.get("postText") or ""
 
@@ -49,19 +50,26 @@ class FacebookScraper(BaseScraper):
             else:
                 post_type = "Post"
 
+            thumbnail = (
+                item.get("photoUrl") or
+                item.get("thumbnailUrl") or
+                item.get("imageUrl") or ""
+            )
+
             er = self.calc_engagement_rate(likes, comments, shares, views=views)
 
             posts.append({
-                "platform": "Facebook",
-                "date": post_date.strftime("%Y-%m-%d"),
-                "type": post_type,
-                "caption": text[:300],
-                "likes": likes,
-                "comments": comments,
-                "shares": shares,
-                "saves": 0,
-                "views": views,
+                "platform":        "Facebook",
+                "date":            post_date.strftime("%Y-%m-%d"),
+                "type":            post_type,
+                "caption":         text[:300],
+                "likes":           likes,
+                "comments":        comments,
+                "shares":          shares,
+                "saves":           0,
+                "views":           views,
                 "engagement_rate": er,
-                "url": item.get("url") or item.get("postUrl") or "",
+                "url":             item.get("url") or item.get("postUrl") or "",
+                "thumbnail":       thumbnail,
             })
         return posts
